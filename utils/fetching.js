0d4c52f9dc40344253442e726b1468e150550b53
santaNan <salazarmiguelito23@gmail.com>
2023-04-24 08:42:02 -0500

    'whabug'

--- utils/fetching.js
+++ utils/fetching.js
@@ -1,8 +1,8 @@
 export default async function sendMessage ({number, titulo}){
-    await fetch(`https://wha-api.up.railway.app/hi`,
+    await fetch(`https://whatsapp-api-macn.onrender.com/hi`,
     {
       method: "POST",
-    //   mode: "no-cors",
+      // mode: "no-cors",
       headers: {
         "Content-Type": "application/json",
         // 'Content-Type': 'application/x-www-form-urlencoded',
